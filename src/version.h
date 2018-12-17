6646a148672b13745b89f11b10b03d17a3d1dcbc
DarkPayCoin <dpcdev@protonmail.com>
2018-12-17 14:39:02 +0100

    Protocol enforcement prepare for Phase2

--- src/version.h
+++ src/version.h
@@ -23,7 +23,7 @@ static const int GETHEADERS_VERSION = 70077;
 
 //! disconnect from peers older than this proto version
 static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT = 70002;
-static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 70004;
+static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 70914;
 
 //! masternodes older than this proto version use old strMessage format for mnannounce
 static const int MIN_PEER_MNANNOUNCE = 70004;
